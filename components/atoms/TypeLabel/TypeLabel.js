70c29fd1a2be4163d0d40230b0c6f8965c78e1e6
Daiane Barizon <daianebarizon22@gmail.com>
2021-01-09 21:04:58 -0300

    style(components): changes style of TypeLabel component

--- components/atoms/TypeLabel/TypeLabel.js
+++ components/atoms/TypeLabel/TypeLabel.js
@@ -4,8 +4,8 @@ import { TypeWrapper } from "./style";
 
 export const TypeLabel = ({ type }) => {
   return (
-    <TypeWrapper color="#FFFFFF" background="#DE2332">
-      {type || "Natural"}
+    <TypeWrapper color="#FFFFFF" background="rgb(187, 255, 211, 0.5)">
+      <p>{type || "Natural"}</p>
     </TypeWrapper>
   );
 };
--- components/atoms/TypeLabel/style.js
+++ components/atoms/TypeLabel/style.js
@@ -1,8 +1,13 @@
 import styled from 'styled-components';
 
 export const TypeWrapper = styled.div`
-    padding: 5px;
-    border-radius: 12px;
+    padding: 5px 20px;
+    border-radius: 50px;
+    margin: 5px;
     color: ${({ color }) => color || "#9f9f9f"};
-    background: ${({ background }) => background || "#242424"};
+    background: ${({ background }) => background || rgb(38, 42, 40, 0.8)};
+
+    p{
+      font-weight: bold;
+    }
 `;
